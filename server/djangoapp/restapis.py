70371eb4017ed337756a8edf279d4433384fc56f
k-raguette <kevin.raguette@gmail.com>
2023-11-25 13:12:34 -0500

    Update restapis.py, get-dealership.js, and reviews.py files

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -2,23 +2,39 @@ import requests
 import json
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
+import os
+
 
 
 # Create a `get_request` to make HTTP GET requests
 def get_request(url, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
+    apikey = kwargs.get("apikey")
     try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
-    except:
-        # If any error occurs
-        print("Network exception occurred")
-    status_code = response.status_code
-    print("With status {} ".format(status_code))
-    json_data = json.loads(response.text)
-    return json_data
+        if apikey:
+            params = dict()
+            params["text"] = kwargs.get("text")
+            params["version"] = kwargs.get("version")
+            params["features"] = kwargs.get("features")
+            params["return_analyzed_text"] = kwargs.get("return_analyzed_text")
+            
+            response = requests.get(url, data=params, auth=HTTPBasicAuth('apikey', apikey), headers={'Content-Type': 'application/json'})
+        else:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'}, params=kwargs)
+        
+        # Check if the response contains valid JSON
+        response.raise_for_status()  # This will raise an exception if the response status code is an HTTP error.
+        json_data = response.json()
+        return json_data
+    except json.JSONDecodeError as e:
+        # Handle the exception and log or print an error message
+        print(f"Error in get_request: {e}")
+        return None
 
 
 # Create a `post_request` to make HTTP POST requests
@@ -96,14 +112,47 @@ def get_dealer_reviews_from_cf(url, **kwargs):
                 sentiment='',
                 id=dealer_review.get("id")
             )
+            sentiment = analyze_review_sentiments(review_obj.review)
+            review_obj.sentiment = sentiment
             results.append(review_obj)
     return results
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
-# - Call get_request() with specified arguments
-# - Get the returned sentiment label such as Positive or Negative
+def analyze_review_sentiments(dealer_review):
+    try:
+        # Attempt to retrieve API key and URL from environment variables
+        apikey = os.environ.get("APINLU")  # export APINLU="apikey_nlu" in terminal
+        url = os.environ.get("URLNLU")  # export URLNLU="url_nlu" in terminal
+
+        # Check if API key and URL are available
+        if not apikey:
+            raise ValueError("API key not found in environment variables.")
+        if not url:
+            raise ValueError("URL not found in environment variables.")
+        
+        authenticator = IAMAuthenticator(apikey)
+        natural_language_understanding = NaturalLanguageUnderstandingV1(
+            version='2022-04-07',
+            authenticator=authenticator
+        )
+
+        natural_language_understanding.set_service_url(url)
+
+        response = natural_language_understanding.analyze(
+            text=dealer_review,
+            language='en',
+            features=Features(sentiment=SentimentOptions(targets=[dealer_review]))
+        ).get_result()
+
+        print(json.dumps(response, indent=2))
+
+        return response["sentiment"]["document"]["label"]
+
+    except Exception as e:
+        # Handle any exceptions that may occur
+        print(f"Error in analyze_review_sentiments: {e}")
+        return None
 
 
 
